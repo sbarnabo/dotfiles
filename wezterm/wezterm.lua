340a0592a06b9520ae2b2c7e5889f17d81377f5a
sbarnabo <scott.barnabo@barnabo-connect.com>
2025-03-04 14:09:14 +0100

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,24 +1,13 @@
 local wezterm = require 'wezterm'
 return {
 	adjust_window_size_when_changing_font_size = false,
-	-- color_scheme = 'termnial.sexy',
 	color_scheme = 'Catppuccin Mocha',
 	enable_tab_bar = false,
 	font_size = 16.0,
 	font = wezterm.font('JetBrains Mono'),
-	-- macos_window_background_blur = 40,
 	macos_window_background_blur = 30,
 	
-	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
-	-- window_background_image_hsb = {
-	-- 	brightness = 0.01,
-	-- 	hue = 1.0,
-	-- 	saturation = 0.5,
-	-- },
-	-- window_background_opacity = 0.92,
 	window_background_opacity = 1.0,
-	-- window_background_opacity = 0.78,
-	-- window_background_opacity = 0.20,
 	window_decorations = 'RESIZE',
 	keys = {
 		{
@@ -40,4 +29,4 @@ return {
 	    action = wezterm.action.OpenLinkAtMouseCursor,
 	  },
 	},
-}#Place Holder
+}
